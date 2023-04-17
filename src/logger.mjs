162ca84cd32ed8ed6a91b70bdc2963a8c25bb1fd
Arne De Belser <debelserarne@hotmail.com>
2023-04-17 20:12:48 +0200

    added logger

---
+++ src/logger.mjs
@@ -0,0 +1,11 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+    level: 'info',
+    format: format.json(),
+    transports: [
+        new transports.File({ filename: 'logs/app.log' })
+    ]
+});
+
+export { logger };
